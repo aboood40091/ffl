6cbac5127f82b1462b6c733b7319a9880679d826
AboodXD <aboood40091@gmail.com>
2023-10-02 21:41:47 +0400

    Complete decompilation of FFLiCreateID functions

---
+++ include/nn/ffl/FFLiCreateID.h
@@ -0,0 +1,56 @@
+#ifndef FFLI_CREATE_ID_H_
+#define FFLI_CREATE_ID_H_
+
+#include <nn/ffl/FFLCreateID.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FFLI_CREATE_ID_BASE_SIZE  (6)
+
+typedef struct FFLiCreateIDBase
+{
+    // Unsure if this is correct
+    union
+    {
+        u8 data[FFLI_CREATE_ID_BASE_SIZE];
+        u16 value16[FFLI_CREATE_ID_BASE_SIZE / sizeof(u16)];
+    };
+}
+FFLiCreateIDBase;
+NN_STATIC_ASSERT(sizeof(FFLiCreateIDBase) == FFLI_CREATE_ID_BASE_SIZE);
+
+#define FFLI_CREATE_ID_FLAG_UNKNOWN     (1 << 4)
+#define FFLI_CREATE_ID_FLAG_TEMPORARY   (1 << 5)
+#define FFLI_CREATE_ID_FLAG_NTR         (1 << 6)
+#define FFLI_CREATE_ID_FLAG_NORMAL      (1 << 7)
+
+typedef struct FFLiCreateID
+{
+    u8                  flags;
+    u8                  _1;
+    u8                  _2;
+    u8                  databaseIndex;
+    FFLiCreateIDBase    base;   // Maybe?
+}
+FFLiCreateID;
+NN_STATIC_ASSERT(sizeof(FFLiCreateID) == FFL_CREATE_ID_SIZE);
+
+BOOL FFLiIsNullMiiID(const FFLCreateID* pCreateID);
+
+void FFLiGetTemporaryMiiID(FFLCreateID* pCreateID);
+
+BOOL FFLiIsNormalMiiID(const FFLCreateID* pCreateID);
+BOOL FFLiIsSpecialMiiID(const FFLCreateID* pCreateID);
+BOOL FFLiIsNTRMiiID(const FFLCreateID* pCreateID);
+BOOL FFLiIsTemporaryMiiID(const FFLCreateID* pCreateID);
+
+BOOL FFLiIsValidMiiID(const FFLCreateID* pCreateID);
+BOOL FFLiIsSameMiiID(const FFLCreateID* a, const FFLCreateID* b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FFLI_CREATE_ID_H_
---
+++ src/FFLiCreateID.cpp
@@ -0,0 +1,105 @@
+#include <nn/ffl/FFLiCreateID.h>
+
+static inline
+const FFLiCreateID* GetCreateID(const FFLCreateID* pCreateID)
+{
+    return reinterpret_cast<const FFLiCreateID*>(pCreateID);
+}
+
+static inline
+const FFLCreateID* GetCreateID(const FFLiCreateID* pCreateID)
+{
+    return reinterpret_cast<const FFLCreateID*>(pCreateID);
+}
+
+BOOL FFLiIsNullMiiID(const FFLCreateID* pCreateID)
+{
+    if (pCreateID != NULL)
+    {
+        const u32 count = FFL_CREATE_ID_SIZE / sizeof(u16);
+        NN_STATIC_ASSERT(count == 5);
+
+        if (pCreateID->value16[0] == 0 &&
+            pCreateID->value16[1] == 0 &&
+            pCreateID->value16[2] == 0 &&
+            pCreateID->value16[3] == 0 &&
+            pCreateID->value16[4] == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return false;
+}
+
+void FFLiGetTemporaryMiiID(FFLCreateID* pCreateID)
+{
+    static const FFLiCreateID temporaryMiiID = {
+        0x20
+    };
+
+    *pCreateID = *GetCreateID(&temporaryMiiID);
+}
+
+BOOL FFLiIsNormalMiiID(const FFLCreateID* pCreateID)
+{
+    u8 flags = GetCreateID(pCreateID)->flags;
+    return (flags & FFLI_CREATE_ID_FLAG_NORMAL) != 0;
+}
+
+BOOL FFLiIsSpecialMiiID(const FFLCreateID* pCreateID)
+{
+    return !FFLiIsNormalMiiID(pCreateID);
+}
+
+BOOL FFLiIsNTRMiiID(const FFLCreateID* pCreateID)
+{
+    u8 flags = GetCreateID(pCreateID)->flags;
+    
+    if (flags & FFLI_CREATE_ID_FLAG_UNKNOWN)
+        return false;
+    
+    if (!(flags & FFLI_CREATE_ID_FLAG_NTR))
+        return false;
+    
+    return true;
+}
+
+BOOL FFLiIsTemporaryMiiID(const FFLCreateID* pCreateID)
+{
+    u8 flags = GetCreateID(pCreateID)->flags;
+    return (flags & FFLI_CREATE_ID_FLAG_TEMPORARY) != 0;
+}
+
+BOOL FFLiIsValidMiiID(const FFLCreateID* pCreateID)
+{
+    return !FFLiIsNullMiiID(pCreateID) && !FFLiIsTemporaryMiiID(pCreateID);
+}
+
+BOOL FFLiIsSameMiiID(const FFLCreateID* a, const FFLCreateID* b)
+{
+    if (FFLiIsValidMiiID(a) && FFLiIsValidMiiID(b))
+    {
+        const u32 count = FFL_CREATE_ID_SIZE / sizeof(u16);
+        NN_STATIC_ASSERT(count == 5);
+
+        if (a->value16[0] == b->value16[0] &&
+            a->value16[1] == b->value16[1] &&
+            a->value16[2] == b->value16[2] &&
+            a->value16[3] == b->value16[3] &&
+            a->value16[4] == b->value16[4])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return false;
+}
