d06f154000767c84c32403f34a58fd685c830967
AboodXD <aboood40091@gmail.com>
2023-10-04 14:33:19 +0400

    Add FFLiSwapEndianImpl() and FFLiSwapEndianArrayImpl()

--- include/nn/ffl/FFLiUtil.h
+++ include/nn/ffl/FFLiUtil.h
@@ -3,6 +3,8 @@
 
 #include <nn/ffl/types.h>
 
+#include <algorithm>
+
 template <typename T>
 T FFLiMin(T a, T b)
 {
@@ -15,6 +17,31 @@ T FFLiMax(T a, T b)
 {
     if (a <= b) return b;
     else        return a;
+}
+
+template <typename T>
+T FFLiSwapEndianImpl(T value)
+{
+    const u32 count = sizeof(T);
+    NN_STATIC_ASSERT(count % 2 == 0);
+    const u32 count_2 = count / 2;
+
+    union {
+        T   value;
+        u8  data[count];
+    } value_ = { .value = value };
+
+    for (u32 i = 0; i < count_2; i++)
+        std::swap(value_.data[i], value_.data[count - 1 - i]);
+
+    return value_.value;
+}
+
+template <typename T>
+void FFLiSwapEndianArrayImpl(T* pArray, u32 size)
+{
+    for (u32 i = 0; i < size; i++)
+        pArray[i] = FFLiSwapEndianImpl<T>(pArray[i]);
 }
 
 void FFLiCopyWcharT2U16(u16* dst, u32 size, const wchar_t* src);
