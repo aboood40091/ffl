103a856baf35dcb6c450a4f5ecfb43aa2163f8cf
AboodXD <aboood40091@gmail.com>
2023-10-06 21:00:19 +0400

    Complete decompilation of FFLiInvalidate functions

--- include/nn/ffl/FFLiInvalidate.h
+++ include/nn/ffl/FFLiInvalidate.h
@@ -6,6 +6,7 @@
 #include <cafe/gx2.h>
 
 void FFLiInvalidate(GX2InvalidateType invalidateType, void* ptr, u32 size);
+void FFLiInvalidateSurface(GX2Surface* pSurface, GX2InvalidateType invalidateType);
 void FFLiInvalidateColorBuffer(GX2ColorBuffer* pColorBuffer);
 void FFLiInvalidateShader(void* ptr, u32 size);
 
