d3280049ac4208dbe8733479d233bec66847dea1
AboodXD <aboood40091@gmail.com>
2023-10-11 15:08:04 +0400

    Decompile FFLiIsVaildResourceHeader()

---
+++ src/FFLiResourceUtil.cpp
@@ -0,0 +1,7 @@
+#include <nn/ffl/FFLiResourceHeader.h>
+#include <nn/ffl/FFLiResourceUtil.h>
+
+FFLResult FFLiIsVaildResourceHeader(const FFLiResourceHeader* pHeader)
+{
+    return pHeader->GetResult();
+}
