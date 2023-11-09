2eb63038d4da6bdc9064b7b1f8496c517746a53f
AboodXD <aboood40091@gmail.com>
2023-11-09 20:30:35 +0400

    Change mlc path into a preprocessor macro

--- src/FFLiPath.cpp
+++ src/FFLiPath.cpp
@@ -4,6 +4,9 @@
 #include <sysapp/title.h>
 #include <nn/save/save.h>
 #else
+#ifndef FFL_MLC_PATH
+#error "Must define FFL_MLC_PATH macro"
+#endif // FFL_MLC_PATH
 #include <cstdio>
 #endif // RIO_IS_CAFE
 
@@ -21,7 +24,7 @@ rio::RawErrorCode FFLiGetResourcePath(char* pDst, u32 size, const char* filename
 #if RIO_IS_CAFE
     return rio::RawErrorCode(SAVEGetSharedDataTitlePath(0x0005001B10056000ULL, filename, pDst, size));
 #else
-    int ret = std::snprintf(pDst, size, "C:/Users/USR/Desktop/cemu_1.7.5/mlc01/sys/title/0005001b/10056000/content/%s", filename);
+    int ret = std::snprintf(pDst, size, FFL_MLC_PATH "/sys/title/0005001b/10056000/content/%s", filename);
     if (ret > 0 && (u32)ret < size)
         return rio::RAW_ERROR_OK;
     return rio::RAW_ERROR_FATAL_ERROR;
@@ -34,7 +37,7 @@ rio::RawErrorCode FFLiGetDatabasePath(char* pDst, u32 size, u64 titleID, const c
     return rio::RawErrorCode(SAVEGetSharedSaveDataPath(titleID, filename, pDst, size));
 #else
     RIO_ASSERT(titleID == 0);
-    int ret = std::snprintf(pDst, size, "C:/Users/USR/Desktop/cemu_1.7.5/mlc01/usr/save/00050010/1004a100/user/common/%s", filename);
+    int ret = std::snprintf(pDst, size, FFL_MLC_PATH "/usr/save/00050010/1004a100/user/common/%s", filename);
     if (ret > 0 && (u32)ret < size)
         return rio::RAW_ERROR_OK;
     return rio::RAW_ERROR_FATAL_ERROR;
