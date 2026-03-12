43ee6aff3f25ef3b6d9852dce06de9dc781528f5
AboodXD <aboood40091@gmail.com>
2026-03-12 12:39:08 +0400

    FFLiRawMaskParts: Minor refactor

--- src/FFLiRawMaskParts.cpp
+++ src/FFLiRawMaskParts.cpp
@@ -102,21 +102,38 @@ void EndianSwap(void* ptr, u32 size)
 
 void InitPrimitive(FFLPrimitiveParam* pPrimitive)
 {
-    const u32 INDEX_BUFFER_SIZE = sizeof(u16) * 4;
+    constexpr u32 INDEX_COUNT = 4;
+    constexpr u32 INDEX_BUFFER_SIZE = sizeof(u16) * INDEX_COUNT;
 
-    static const u16 INDEX_BUFFER[4] = { 2, 1, 3, 0 };
+    static const u16 INDEX_BUFFER[INDEX_COUNT] = { 2, 1, 3, 0 };
     NN_STATIC_ASSERT(sizeof(INDEX_BUFFER) == INDEX_BUFFER_SIZE);
 
     pPrimitive->primitiveType = rio::Drawer::TRIANGLE_STRIP;
-    pPrimitive->indexCount = 4;
+    pPrimitive->indexCount = INDEX_COUNT;
     pPrimitive->pIndexBuffer = FFLiBugVgtFixedIndexPtr(Allocate(FFLiBugCanVgtFixedIndexSize(FFLiBugCanSwapSize(INDEX_BUFFER_SIZE)), rio::Drawer::cIdxAlignment));
 
     rio::MemUtil::copy(pPrimitive->pIndexBuffer, INDEX_BUFFER, INDEX_BUFFER_SIZE);
     EndianSwap(pPrimitive->pIndexBuffer, INDEX_BUFFER_SIZE);
 }
 
+void MTX44MultVec(const rio::BaseMtx44f& mtx, const FFLVec3& src, FFLVec3& dst)
+{
+    f32 x = mtx.m[0][0] * src.x + mtx.m[0][1] * src.y + mtx.m[0][2] * src.z + mtx.m[0][3];
+    f32 y = mtx.m[1][0] * src.x + mtx.m[1][1] * src.y + mtx.m[1][2] * src.z + mtx.m[1][3];
+    f32 z = mtx.m[2][0] * src.x + mtx.m[2][1] * src.y + mtx.m[2][2] * src.z + mtx.m[2][3];
+    f32 w = mtx.m[3][0] * src.x + mtx.m[3][1] * src.y + mtx.m[3][2] * src.z + mtx.m[3][3];
+
+    f32 w_inv = 1.0f / w;
+
+    dst.x = x * w_inv;
+    dst.y = y * w_inv;
+    dst.z = z * w_inv;
+}
+
 void CalcAttribute(FFLVec4* pPosBuf, FFLVec2* pTexBuf, FFLiOriginPosition originPosition, const rio::BaseMtx44f* pMVPMatrix)
 {
+    constexpr u32 VERTEX_COUNT = 4;
+
     f32 posXAdd = 0.0f;
     f32 texCoordX01 = 0.0f;
     f32 texCoordX23 = 0.0f;
@@ -136,11 +153,11 @@ void CalcAttribute(FFLVec4* pPosBuf, FFLVec2* pTexBuf, FFLiOriginPosition origin
         break;
     }
 
-    f32 posX[4] = { 1.0f, 1.0f, 0.0f, 0.0f };
-    f32 posY[4] = { -0.5f, 0.5f, 0.5f, -0.5f };
-    f32 texCoordY[4] = { 0.0f, 1.0f, 1.0f, 0.0f };
+    static const f32 posX[VERTEX_COUNT] = { 1.0f, 1.0f, 0.0f, 0.0f };
+    static const f32 posY[VERTEX_COUNT] = { -0.5f, 0.5f, 0.5f, -0.5f };
+    static const f32 texCoordY[VERTEX_COUNT] = { 0.0f, 1.0f, 1.0f, 0.0f };
 
-    for (u32 i = 0; i < 4; i++)
+    for (u32 i = 0; i < VERTEX_COUNT; i++)
     {
         pPosBuf[i].x = posX[i] + posXAdd;
         pPosBuf[i].y = posY[i];
@@ -155,24 +172,15 @@ void CalcAttribute(FFLVec4* pPosBuf, FFLVec2* pTexBuf, FFLiOriginPosition origin
     pTexBuf[2].x = texCoordX23;
     pTexBuf[3].x = texCoordX23;
 
-    for (u32 i = 0; i < 4; i++)
-    {
-        const f32 w = pMVPMatrix->m[3][0] * pPosBuf[i].x + pMVPMatrix->m[3][1] * pPosBuf[i].y + pMVPMatrix->m[3][2] * pPosBuf[i].z + pMVPMatrix->m[3][3];
-        const f32 w_inv = 1 / w;
-
-        pPosBuf[i] = FFLVec4 {
-            (pMVPMatrix->m[0][0] * pPosBuf[i].x + pMVPMatrix->m[0][1] * pPosBuf[i].y + pMVPMatrix->m[0][2] * pPosBuf[i].z + pMVPMatrix->m[0][3]) * w_inv,
-            (pMVPMatrix->m[1][0] * pPosBuf[i].x + pMVPMatrix->m[1][1] * pPosBuf[i].y + pMVPMatrix->m[1][2] * pPosBuf[i].z + pMVPMatrix->m[1][3]) * w_inv,
-            (pMVPMatrix->m[2][0] * pPosBuf[i].x + pMVPMatrix->m[2][1] * pPosBuf[i].y + pMVPMatrix->m[2][2] * pPosBuf[i].z + pMVPMatrix->m[2][3]) * w_inv,
-            pPosBuf[i].w
-        };
-    }
+    for (u32 i = 0; i < VERTEX_COUNT; i++)
+        MTX44MultVec(*pMVPMatrix, reinterpret_cast<const FFLVec3&>(pPosBuf[i].x), reinterpret_cast<FFLVec3&>(pPosBuf[i].x));
 }
 
 void InitAttributes(FFLAttributeBufferParam* pAttributes, FFLiOriginPosition originPosition, const rio::BaseMtx44f* pMVPMatrix)
 {
-    const u32 POSITION_BUFFER_SIZE = sizeof(FFLVec4) * 4;
-    const u32 TEXCOORD_BUFFER_SIZE = sizeof(FFLVec2) * 4;
+    constexpr u32 VERTEX_COUNT = 4;
+    constexpr u32 POSITION_BUFFER_SIZE = sizeof(FFLVec4) * VERTEX_COUNT;
+    constexpr u32 TEXCOORD_BUFFER_SIZE = sizeof(FFLVec2) * VERTEX_COUNT;
 
     static const FFLAttributeBufferParam ATTRIBUTES = { {
         { POSITION_BUFFER_SIZE, sizeof(FFLVec4) },
@@ -238,9 +246,10 @@ void InvalidateAttributes(FFLAttributeBufferParam* pAttributes)
 
 void InitAttributesForFill(FFLAttributeBufferParam* pAttributes)
 {
-    const u32 POSITION_BUFFER_SIZE = sizeof(FFLVec4) * 4;
+    constexpr u32 VERTEX_COUNT = 4;
+    constexpr u32 POSITION_BUFFER_SIZE = sizeof(FFLVec4) * VERTEX_COUNT;
 
-    static const FFLVec4 POSITION_BUFFER[4] = {
+    static const FFLVec4 POSITION_BUFFER[VERTEX_COUNT] = {
         {  1.0f, -1.0f,  0.0f,  0.0f },
         {  1.0f,  1.0f,  0.0f,  0.0f },
         { -1.0f,  1.0f,  0.0f,  0.0f },
